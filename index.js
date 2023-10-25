09ef178c7b5319375861f9551466552a9adf400d
dmarie777 <deimarie777@gmail.com>
2023-10-24 21:17:19 -0500

    Addition of morgan middleware

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+app.use(morgan('tiny'))
+morgan.token('req', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req'))
+
 let persons = [
     { 
       "id": 1,
