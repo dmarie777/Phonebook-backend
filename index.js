f08e0c040ef93021785f78a556f8eba16dc1fc14
dmarie777 <deimarie777@gmail.com>
2023-10-31 16:35:50 -0500

    Added error handler middleware

--- index.js
+++ index.js
@@ -23,21 +23,27 @@ app.get('/info', (request, response) => {
     <p>${new Date()}</p>`)
 })
 
-app.get('/api/persons/:id', (request, response) =>{
-    Person.findById(request.params.id) .then (result => {
-      response.send(result);
+app.get('/api/persons/:id', (request, response, next) =>{
+    Person.findById(request.params.id) .then (person => {
+      if (person) {
+        response.send(person);
+      }
+      else {
+        response.status(404).end()
+      }
     })
-    .catch( err=> {
-      response.status(404).end()
-    })
+    .catch( err=> next(err))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
       .then(result => {
         response.status(204).end()
       })
+      .catch(error => next(error))
 })
+
+
 
 
 app. post('/api/persons', (request, response) => {
@@ -66,6 +72,15 @@ app. post('/api/persons', (request, response) => {
 
   })
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT 
 app.listen(PORT, () => {
