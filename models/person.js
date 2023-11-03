fce6123ebe619af2d1318513106961a5f9b10e1b
dmarie777 <deimarie777@gmail.com>
2023-11-02 20:29:39 -0500

    Added custom validation for phone number

--- models/person.js
+++ models/person.js
@@ -11,8 +11,23 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+    },
+    number: {
+        type: String,
+        minLength:8,
+        validate: {
+            validator: function(v) {
+                return v.split('-').length ===2?  
+                v.split('-').map((e,i) => (i === 0 && e.length >= 2 && e.length < 4  && typeof +e === 'number') || (i === 1 && typeof +e === 'number' )? true:false ).every(e=> e===true)
+                : false
+            },
+            message: props => `${props.value} is not a valid phone number`
+        },
+        required: [true, 'User phone number required']
+    },
 })
 
 personSchema.set('toJSON', {
